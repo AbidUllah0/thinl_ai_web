f0d51ed7698bb2c2116bd6a0b74ec2ecada26e44
israrali05 <105581953+israrali05@users.noreply.github.com>
2023-11-22 09:15:30 +0500

    new add login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:project22/screen/Login/login_page.dart';
 import 'package:project22/screen/drawer/drawer.dart';
 
-
-
 void main() {
   runApp(const MyApp());
 }
@@ -13,19 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        final height = MediaQuery.of(context).size.height;
-        final width = MediaQuery.of(context).size.width;
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Flutter MediaQuery Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home:   LoginScreen(),
-        );
-      },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter MediaQuery Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
     );
   }
 }
--- lib/screen/Login/login_page.dart
+++ lib/screen/Login/login_page.dart
@@ -222,7 +222,10 @@ class _LoginScreenState extends State<LoginScreen> {
                               ),
                               ElevatedButton.icon(
                                 onPressed: () {
-                                  Get.to(() => CustomDrawer());
+                                Navigator.push(
+  context,
+  MaterialPageRoute(builder: (context) => CustomDrawer()),
+);
                                 },
                                 icon: Image.asset(
                                   AppImages.login,
@@ -361,9 +364,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                 height: screenHeight * 0.03,
                               ),
                               ElevatedButton.icon(
-                                onPressed: () {
-                                  Get.to(() => CustomDrawer);
-                                },
+                                onPressed: () {},
                                 icon: Image.asset(
                                   AppImages.addUser,
                                   scale: screenWidth * 0.02,
